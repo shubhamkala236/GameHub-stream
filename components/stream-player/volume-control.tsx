c8b6f3fb00c302594ce83fdc807c3955d0b0103b
lawkk <lawkktd@gmail.com>
2023-12-29 22:09:37 +0530

    added video player and volume setting

---
+++ components/stream-player/volume-control.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Volume1, Volume2, VolumeX } from "lucide-react";
+import { Hint } from "../hint";
+import { Slider } from "../ui/slider";
+
+interface VolumeControlProps {
+  onToggle: () => void;
+  onChange: (value: number) => void;
+  value: number;
+}
+
+
+export const VolumeControl = ({onChange,onToggle,value}:VolumeControlProps) => {
+    const isMuted = value === 0;
+    const isAboveHalf = value > 50;
+
+    let Icon = VolumeX;
+
+    if(!isMuted && isAboveHalf)
+    {
+        Icon = Volume2;
+    }
+    else if( !isMuted  )
+    {
+        Icon = Volume1
+    }
+
+    const label = isMuted ? "Unmute" : "Mute";
+    
+    const handleChange = (value:number[]) => {
+        onChange(value[0]);
+    }
+    
+    return (
+        <div className="flex items-center gap-2">
+            <Hint label={label} asChild>
+                <button onClick={onToggle} className="text-white hover:bg-white/10 p-1.5 rounded-lg">
+                    <Icon className="h-6 w-6"/>
+                </button>
+            </Hint>
+            <Slider
+                className="w-[8rem] cursor-pointer"
+                onValueChange={handleChange}
+                value={[value]}
+                max={100}
+                step={1}
+            />
+        </div>
+    )
+}
